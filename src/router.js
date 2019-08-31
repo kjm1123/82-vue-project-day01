83f11b4231e1bf335d70b34ee9d925eb92ecbc25
kjm <kjm@qq.com>
2019-08-31 21:38:40 +0800

    初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Home from './views/home.vue'
+import Login from './views/login.vue/index.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      component: Login
+    },
     {
       path: '/',
       name: 'home',
--- src/views/home.vue/index.vue
+++ src/views/home.vue/index.vue
@@ -1,15 +1,12 @@
 <template>
-    <div class="home">
-
-    </div>
+<div>
+    home
+</div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  components: {
 
-  }
 }
 </script>
 
