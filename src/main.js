a91451b92b9027bb8f6b99fb9bffe332686d6407
kjm <kjm@qq.com>
2019-08-31 20:42:33 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化项目,包括padding和 margin
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册ElementUI组件,可以在任意位置使用
 new Vue({
--- src/router.js
+++ src/router.js
@@ -10,17 +10,17 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // components  后面可以很组件 也可以跟函数
-      // spa 缺点  首次加载慢  按需加载
-      // 点击导航  跳转到about 时才回去加载about  而且只有一次
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   // components  后面可以很组件 也可以跟函数
+    //   // spa 缺点  首次加载慢  按需加载
+    //   // 点击导航  跳转到about 时才回去加载about  而且只有一次
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
