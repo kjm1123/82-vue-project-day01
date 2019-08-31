230ac8b668b5f368312392480ec2278c02b19d07
kjm <kjm@qq.com>
2019-08-31 20:11:56 +0800

    安装ElementUI

--- src/router.js
+++ src/router.js
@@ -17,6 +17,9 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      // components  后面可以很组件 也可以跟函数
+      // spa 缺点  首次加载慢  按需加载
+      // 点击导航  跳转到about 时才回去加载about  而且只有一次
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
