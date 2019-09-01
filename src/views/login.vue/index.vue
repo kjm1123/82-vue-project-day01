d6202114fba83183f8f1e20ec12be4a3feae789f
kjm <kjm@qq.com>
2019-09-01 10:00:36 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/login.vue/index.vue
+++ src/views/login.vue/index.vue
@@ -1,16 +1,81 @@
 <template>
-<div>
-    login
+<div class="login">
+   <el-card class="login-card">
+   <div class="logo">
+       <img src="../../assets/images/img/logo_index.png" alt="">
+   </div>
+   <!-- 表单组件 -->
+    <!-- 数据校验,首先要给el-from  一个model属性  表示数据对象 -->
+   <el-form :model=" loginForm" :rules= "loginForm" style="margin-top:20px">
+       <!-- 表单项 -->
+            <el-form-item prop="mobile">
+                <!-- 放置组件内容 -->
+                <!-- prpo 绑定需要校验的字段名 但是不写loginForm.mobile,只写mobile-->
+                <el-input v-model = "loginForm.mobile" placeholder="请输入手机号">  </el-input>
+
+            </el-form-item>
+            <el-form-item prop="code">
+                <!-- //绑定验证码 -->
+                <el-input v-model = "loginForm.code" style="width:250px" placeholder="请输入验证码"></el-input>
+                <el-button style="float:right">发送验证码</el-button>
+            </el-form-item>
+             <el-form-item class="check">
+                 <!-- 绑定协议 -->
+                 <el-checkbox v-model = "loginForm.check">我已同意和阅读</el-checkbox>
+                 </el-form-item>
+            <el-form-item>
+               <el-button  type="primary" style="width:100%">登陆</el-button>
+            </el-form-item>
+   </el-form>
+   </el-card>
 </div>
 
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否同意勾选
+      },
+      //   定义rules 检验规则  表单是根据规则去校验  没有规则  就没有校验
+      // 它的格式为 : key(字段名) :value(数组对象) => 多个 => 一个字段,可能有一个或多个校验规则
+      loginRules: {
+        mobile: [{
+          required: true, // 意味着必填
+          message: '手机号不能为空' // 意味着手机号不能为空
+        }]
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+//如果我们要用less/scss等预处理css语言的话,需要给一个lang属性
+//lang属性  是对css进行语言指定
+//是从碰到 只针对当前的组建样式起作用
+// 100vh  就是占据当前可视屏幕的100%
+.login {
+    background-image: url('../../assets/images/img/login_bg.jpg');
+    height:100vh;
+    background-size:cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+    width: 440px;
+    height: 340px;
+    .logo{
+        text-align: center;
+        img {
+            height: 45px;
+        }
+    }
+}
+}
 
 </style>
