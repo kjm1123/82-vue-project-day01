2671d6d055d237d0d95f3b773e7c271fb5252aef
kjm <kjm@qq.com>
2019-09-02 19:40:19 +0800

    黑马头条PC-主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -7,48 +7,48 @@
 
       background-color="#323745"
       text-color="#adafb5"
-      active-text-color="#ffd04b">
+      active-text-color="#ffd04b" router>
 
       <el-menu-item index="2">
         <i class="el-icon-s-home"></i>
         <span slot="title">首页</span>
       </el-menu-item>
         <!-- <el-submenu>是有折叠的 -->
-          <el-submenu>
+          <el-submenu index ="content">
             <!-- <el-submenu>定义具名插槽 title -->
             <template slot="title">
                 <i class="el-icon-s-shop"></i>
                   <span>内容管理</span>
                </template>
-            <el-menu-item>
+            <el-menu-item index="/home/pubilish">
                 发布文章
                  </el-menu-item>
-                 <el-menu-item>
+                 <el-menu-item index="/home/articles">
                 内容列表
                  </el-menu-item>
-                  <el-menu-item>
+                  <el-menu-item index="/home/comment">
                 评论管理
                  </el-menu-item>
-                  <el-menu-item>
+                  <el-menu-item index="/home/material">
                 素材管理
                  </el-menu-item>
              </el-submenu>
-               <el-submenu>
+               <el-submenu index ="fans">
             <!-- <el-submenu>定义具名插槽 title -->
             <template slot="title">
                 <i class="el-icon-s-custom"></i>
                   <span>粉丝管理</span>
                </template>
-            <el-menu-item>
+            <el-menu-item index ="/home/fansdata">
                 图文数据
                  </el-menu-item>
-                 <el-menu-item>
+                 <el-menu-item index ="/home/fansinfo">
                 粉丝概况
                  </el-menu-item>
-                  <el-menu-item>
+                  <el-menu-item index ="/home/fanslist">
                 粉丝列表
                  </el-menu-item>
-                  <el-menu-item>
+                  <el-menu-item index ="/home/fansimg">
                 粉丝画像
                  </el-menu-item>
              </el-submenu>
