d9c2075240647a33ce98603d7e93aea3f9084b2e
kjm <kjm@qq.com>
2019-09-03 21:23:07 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- separator代表 他们之间用 > 来分割 -->
+    <el-breadcrumb separator=">">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>
+      <!-- <定义一个插槽> -->
+      <slot name="title"></slot>
+  </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 // 引入组件
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
+import breadCrumb from './common/bread-crumb.vue'
 // 所有自定义的组建的插件(全局注册)
 export default {
   install: function (Vue) {
     Vue.component('layout-aside', layoutAside) // 注册左侧导航事件
     Vue.component('layout-header', layoutHeader) // 注册头部组建
+    Vue.component('bread-crumb', breadCrumb) // 注册面包屑
   }
 }
