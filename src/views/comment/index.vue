d533902847beac17519ccc98081b0220a761ea68
kjm <kjm@qq.com>
2019-09-04 19:10:29 +0800

    黑马头条PC-评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,15 +1,30 @@
 <template>
-<div>
-    评论列表
-</div>
+  <!-- 最外层用 el-card 卡片做页面 -->
+  <el-card>
+    <!-- 利用面包屑的标签(具名插槽) -->
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <!-- ----表格  这里边需要数据 -->
+    <el-table :data = 'tableData'>
+      <el-table-column width ="700px" label="标题" prop="name"> </el-table-column>
+      <el-table-column  label="评论状态" prop="date"> </el-table-column>
+      <el-table-column label="总评论数" prop="address"> </el-table-column>
+       <el-table-column  label="粉丝评论数" prop="date"> </el-table-column>
+      <el-table-column label="操作" prop="address"> </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
