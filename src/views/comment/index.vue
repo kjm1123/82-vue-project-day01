a5cffe92f8949a7850d06caef4cb95b6aa1e9f83
kjm <kjm@qq.com>
2019-09-05 14:46:16 +0800

    黑马头条pc-评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -8,7 +8,7 @@
     <!-- ----表格  这里边需要数据 -->
     <el-table :data="list">
       <!-- formatter 是 el-table-cllumn 属性 -->
-      <el-table-column width="700px" label="标题" prop="title"></el-table-column>
+      <el-table-column width="500px" label="标题" prop="title"></el-table-column>
       <el-table-column :formatter="formatter" label="评论状态" prop="comment_status"></el-table-column>
       <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
       <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
@@ -21,17 +21,40 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-row type="flex" justify="center" style="margin:10px 0">
+        <!-- //分页组件  current-page 当前页码  page-size每页显示多少条  total  表示总的页数 -->
+         <el-pagination   @current-change="changePage"  :current-page = "page.page" :page-size = "page.pageSize" :total = "page.total"
+  background
+  layout="prev, pager, next" >
+</el-pagination>
+    </el-row>
+
   </el-card>
+
 </template>
 
 <script>
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        page: 1, // 当前页码
+        pageSize: 10, // 当前条数
+        total: 0 // 当前总数
+      }
+
     }
   },
   methods: {
+    changePage (newPage) {
+      // alert(newPage)
+      // 给当前页面最心值
+      this.page.page = newPage
+      this.getComments()
+    },
+
     openOrClose (row) {
       // 打开或关闭评论
       let mess = row.comment_status ? '关闭' : '打开'
@@ -40,15 +63,11 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.$message({
-          type: 'success',
-          message: '删除成功!'
-        })
         // 写接口
         this.$axios({
           method: 'put',
           url: '/comments/status',
-          params: { article_id: row.id }, // 传过article 参数
+          params: { article_id: row.id.toString() }, // 传过article 参数
           data: { allow_comment: !row.comment_status } // 取反  因为当前是true,只能改成false   当前是false的话,只能改成true
         }).then(result => {
           this.getComments() // 成功之后  重新调用  拉取数据 ==> 前后台同步
@@ -65,11 +84,12 @@ export default {
       // body 路径参数 data
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.page, per_page: this.page.pageSize }
       }).then(result => {
         // console.log(result.data)
         // 再把打印出来的数据给了当前的list
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
